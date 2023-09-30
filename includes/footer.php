906f5f62babf278658d2cb971171dc887451bd9b
AngryChineseWoman <146582804+AngryChineseWoman@users.noreply.github.com>
2023-09-30 12:04:11 -0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<p>Content created by Kathy Yen for INFO 240 Spring 2021
+
+</p>
---
+++ includes/navigation.php
@@ -0,0 +1,7 @@
+   <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="review.php">Reviews</a></li>
+      <li><a href="resources.php">Resources</a></li>
+      <li><a href="form.php">Nominate</a></li>
+      <li><a href="about.php">About</a></li>
+   </ul>
