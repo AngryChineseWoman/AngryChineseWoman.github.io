8c99434cd588d4cf6664970dbcea975138246ff6
AngryChineseWoman <146582804+AngryChineseWoman@users.noreply.github.com>
2023-09-30 12:00:51 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Young Adult Literature</title>
+  <meta name="description" content="Main page for Young Adult Literature website">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" media="screen" href="css/style.css">
+  <link rel="stylesheet" media="print" href="css/print.css">
+  
+  
+</head> 
+
+  <body>
+  
+	<div class="wrapper">
+
+
+		<header class="banner">
+   		<h1>Young Adult Literature</h1>
+   		
+   		   	<img class="fiction" src="images/fiction1.jpg" alt="fictionsign" width="450" height="400">   		
+   		
+		</header> <!-- end of header -->
+
+        <nav class="primary-menu">
+		<?php include('includes/navigation.php');?>
+		</nav>
+
+
+	    <div class="content-wrapper">
+
+		<main>
+		<h2>Welcome! We love Young Adult Literature</h2>
+		
+		
+<p>
+Hi! and I am really happy that you found my site. While in the early stages of development (think not-quite ready for TK yet),
+my hope is to grow this site over time with more book reviews, resources, and interviews with authors or other figures
+in Young Adult Literature. For now, to get things started, we will pick an author to showcase for a month or so, and provide
+a few ideas on how you can find new books to explore.
+</p>
+<p>
+And if you are in the mood to explore, Young Adult Literature (aka "YALit") has its own hashtag on Twitter and 
+over 1 milllion posts on Instagram. If you already spend some quality time on Instagram, check out the #yalit scene. 
+I bet you'll find some books and comments (of course with the comments) that will catch your eye.
+There's a couple other ideas on how to find new books on the <a href="resources.php" target="_self">Resources</a> page of this site.
+</p>
+
+<p>Young readers today have so many books to choose from for enjoyment. There are whole sections in libraries dedicated to graphic novels, 
+bringing this genre to a more mainstream audience. Authors are feeding off the realization that younger readers enjoy series
+and characters that can span 7 or 8 books' worth of content. And, while it may been a long time in coming,
+it is easier than ever to find books with characters that represent a wide spectrum of personalities, shapes, genders, and
+skin colors. This subject area has grown significantly 
+<a href="https://slate.com/culture/2019/12/decade-in-young-adult-fiction.html" target="_blank">in recent years.</a>  But keeping
+that momentum without cranking out books' for the sake of it, could be a challenge.
+</p>
+
+<p> I hope you enjoy the site and find it useful. Come back again to check out new content!
+</p>
+
+
+        </main>  <!-- end of main -->
+
+ 		<aside class="sidebar"> 
+
+		<h2 id="yadefine">What is a Young Adult?</h2>
+<p>
+"Young adult" is an interesting term and can be a little hard to pin down. Merriam-Webster's <a href="https://www.merriam-webster.com/dictionary/young%20adult" target="_blank">
+defintion</a> specifically references the term relative to how it is used in literature. It goes on to reference "adolescent"
+and leaves it at that. The term from a more "biological" perspective can yield a fairly wide range of years. 
+The World Health Organization, gives a range of <a href="https://www.who.int/southeastasia/health-topics/adolescent-health
+" target="_blank">10 to 25 years of age. </a>
+</p>
+
+<p>
+Because the definition of a "young adult" can span just a large number of years, jumping in to any book in the YA section
+of the bookstore does not guarantee that you'll love it. If you are at one end of the of spectrum, the subject matter
+that you find interesting could be very different than what someone at the other end is currently in to. Regardless of
+which end of the range you are in today, keep looking for that next, new, favorite book. I'm sure it's out there. 
+</p>
+
+
+        </aside> <!-- end of aside -->
+
+
+		</div> <!-- end of content-wrapper -->
+
+<a href="#">Back to Top</a>
+
+
+<div class="container">
+
+	<div class="one">Fantasy</div>
+	<div class="two">Historical Fiction</div>
+	<div class="three">SciFi</div>
+	<div class="four">Mystery</div>
+	<div class="five">Sports</div>
+
+</div> <!-- end of container div -->
+
+
+
+	<footer class="site-footer">
+
+	<?php include('includes/footer.php');?>
+
+	</footer>
+
+
+
+
+
+	</div> <!-- end of wrapper -->
+
+   </body> <!-- end of body -->
+</html>
